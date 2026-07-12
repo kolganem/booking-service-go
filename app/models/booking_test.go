0d4398ae8c6431f7ce9ec0edc1a559f16a8eafbe
Alex Kolh <a.kolgan@dppm.pro>
2026-07-12 15:58:29 +0300

    fix tests

--- app/models/booking_test.go
+++ app/models/booking_test.go
@@ -85,6 +85,8 @@ func TestCancel_FromConfirmed_PastStartDate_Error(t *testing.T) {
 		time.Now().AddDate(0, 0, -3),
 		time.Now().AddDate(0, 0, -1),
 		time.Now().AddDate(0, 0, -5),
+		"",
+		time.Time{},
 	)
 
 	err := b.Cancel(time.Now())
